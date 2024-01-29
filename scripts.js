cbedf0c3ea627cb682014afe4657d717c6c22516
ledangtienhung237 <154294569+ledangtienhung237@users.noreply.github.com>
2024-01-30 01:39:58 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,43 +24,49 @@ function loadSheetData() {
 }
 
 function displaySheetData(data) {
-  var sheetDataElement = document.getElementById('sheetData');
-
-  if (data && data.length > 0) {
-    var table = '<table>';
-    
-    // Add a header row with background color
-    table += '<tr>';
-    for (var i = 0; i < data[0].length; i++) {
-      table += '<th>' + data[0][i] + '</th>';
-    }
-    table += '</tr>';
-
-    // Add data rows with background color coding
-    for (var i = 1; i < data.length; i++) {
+    var sheetDataElement = document.getElementById('sheetData');
+  
+    if (data && data.length > 0) {
+      var table = '<table>';
+      
+      // Add a header row with background color
       table += '<tr>';
-      for (var j = 0; j < data[i].length; j++) {
-        var cellValue = data[i][j];
-        var cellClass = '';
-
-        // Apply background color coding based on cell value
-        if (cellValue === 'N') {
-          cellClass = 'grey-bg';
-        } else if (cellValue === 'D') {
-          cellClass = 'lightly-blue-bg';
-        } else if (cellValue === 'OFF') {
-          cellClass = 'red-bg';
-        }
-
-        table += '<td class="' + cellClass + '">' + cellValue + '</td>';
+      for (var i = 0; i < data[0].length; i++) {
+        table += '<th>' + data[0][i] + '</th>';
       }
       table += '</tr>';
+  
+      // Add data rows with background color coding
+      for (var i = 1; i < data.length; i++) {
+        table += '<tr>';
+        for (var j = 0; j < data[i].length; j++) {
+          var cellValue = data[i][j];
+          var cellClass = '';
+  
+          // Apply background color coding based on cell value
+          if (cellValue === 'N') {
+            cellClass = 'grey-bg';
+          } else if (cellValue === 'D') {
+            cellClass = 'lightly-blue-bg';
+          } else if (cellValue === 'OFF') {
+            cellClass = 'red-bg';
+          }
+  
+          // Apply bold styling if the content is in the list
+          if (["Hunter", "Alvis", "Paul", "Nancy", "Duncan", "Marcel", "Cris"].includes(cellValue)) {
+            table += '<td class="' + cellClass + '"><b>' + cellValue + '</b></td>';
+          } else {
+            table += '<td class="' + cellClass + '">' + cellValue + '</td>';
+          }
+        }
+        table += '</tr>';
+      }
+      
+      table += '</table>';
+      
+      sheetDataElement.innerHTML = table;
+    } else {
+      sheetDataElement.innerHTML = 'No data available';
     }
-    
-    table += '</table>';
-    
-    sheetDataElement.innerHTML = table;
-  } else {
-    sheetDataElement.innerHTML = 'No data available';
   }
-}
+  
